f8faf05843b2b23643b1436e77c061feb7362078
Joaquin <joaquin@isla.io>
2014-03-04 11:11:09 +0100

    Add XML layout, listeners and adapters
    Add XML layout, listeners and adapters
    
    Contact: joaquin@isla.io

--- app/src/main/java/com/example/android_todo/app/ToDoList.java
+++ app/src/main/java/com/example/android_todo/app/ToDoList.java
@@ -2,8 +2,15 @@ package com.example.android_todo.app;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ToDoList extends ActionBarActivity {
 
@@ -11,6 +18,28 @@ public class ToDoList extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_to_do_list);
+        ListView myListView = (ListView) findViewById(R.id.myListView);
+        final EditText myEditText = (EditText) findViewById(R.id.myEditText);
+
+        final ArrayList<String> todoItems = new ArrayList<String>();
+        final ArrayAdapter<String> aa;
+        aa = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, todoItems);
+        myListView.setAdapter(aa);
+
+        myEditText.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if(event.getAction() == KeyEvent.ACTION_DOWN){
+                    if(keyCode == KeyEvent.KEYCODE_ENTER){
+                        todoItems.add(0, myEditText.getText().toString());
+                        aa.notifyDataSetChanged();
+                        myEditText.setText("");
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
     }
 
 
